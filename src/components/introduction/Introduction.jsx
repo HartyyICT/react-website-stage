561ef0a2badd00d95ce4459dd464f4849895569f
HartyyICT <2104872asep@zuyd.nl>
2024-11-06 14:27:30 +0100

    papier toegevoegd

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Parallax, Parralax } from 'react-parallax';
 import './IntroductionStyles.css';
-import Ingang from '../../assets/ingang.jpg';
+
 
 const Introduction = () => {
   return (
-    <Parallax className='image' bgImage={Ingang} strength={800}>
       <div name='introduction' className='introduction'>
         <div className="container">
           <div className="top">
@@ -16,7 +14,6 @@ const Introduction = () => {
           </div>
         </div>
       </div>
-    </Parallax>  
   );
 };
 
